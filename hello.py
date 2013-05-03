cf5124e8790a7e5c7cfc011a29990c5dc700357e
ttc9082 <tt1068@students.poly.edu>
2013-05-02 20:20:21 -0400

    lat and lng

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ import os
 
 
 import urllib
+import json
 import webapp2
 
 from google.appengine.ext import db
@@ -26,64 +27,49 @@ MAIN_PAGE_FOOTER_TEMPLATE = """\
 </html>
 """
 
-class Greeting(db.Model):
-    """Models an individual Guestbook entry with author, content, and date."""
-    author = db.StringProperty()
-    content = db.StringProperty(multiline=True)
+class twitter(db.Model):
+    results = db.StringProperty(multiline=True)
     date = db.DateTimeProperty(auto_now_add=True)
 
-
-def guestbook_key(guestbook_name=None):
+def twitter_key(twitter_name=None):
     """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
-    return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
-
+    return db.Key.from_path('twitter', twitter_name or 'default')
 
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
-        guestbook_name=self.request.get('guestbook_name')
-        greetings_query = Greeting.all().ancestor(
-            guestbook_key(guestbook_name)).order('-date')
-        greetings = greetings_query.fetch(10)
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
         template_values = {
-            'greetings': greetings,
-            'url': url,
-            'url_linktext': url_linktext,
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
-
-
-class Guestbook(webapp2.RequestHandler):
-
     def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each greeting
-        # is in the same entity group. Queries across the single entity group
-        # will be consistent. However, the write rate to a single entity group
-        # should be limited to ~1/second.
-        guestbook_name = self.request.get('guestbook_name')
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
+        category = self.request.get('category')
+        address = self.request.get('location')
+        geourl = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + '1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=true'
+        get = urllib.urlopen(geourl)
+        output_json = json.load(get)
+        lat = output_json['results'][0]['geometry']['location']['lat']
+        lng = output_json['results'][0]['geometry']['location']['lng']
+        '''
+        key_name = category + address
+        self.response.write(category)
+        twi = twitter(parent=twitter_key(key_name))
+        twi.results = self.request.get('content')
+        twi.put()
+        twitters = db.GqlQuery("SELECT * "
+                                "FROM twitter "
+                                "WHERE ANCESTOR IS :1 "
+                                "ORDER BY date DESC LIMIT 10",
+                                twitter_key(key_name))
+        twitters'''
+        template_values = {
+            'lat': lat,
+            'lng': lng,    
+        }
 
-        if users.get_current_user():
-            greeting.author = users.get_current_user().nickname()
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
 
-        greeting.content = self.request.get('content')
-        greeting.put()
-
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
-
-
-app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/sign', Guestbook)],
+app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
